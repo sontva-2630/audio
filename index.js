b5b3aa08393deb2dcddd9530366dc941c1ac7fe6
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2021-09-01 12:58:14 +0700

    rm no

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
 const part = require("path");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(part.join(__dirname + "/audio.html"));
 });
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("App run on ", PORT);
